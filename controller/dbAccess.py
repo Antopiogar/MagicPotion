9595a3e177978bb09c49aa6b7823ff41ffcb06a3
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2022-11-01 18:48:51 +0100

    Update dbAccess.py

--- controller/dbAccess.py
+++ controller/dbAccess.py
@@ -1,10 +1,35 @@
 from model.Ingredient import Ingredient
+from model.user import user
 import mysql.connector
 
 class DbAccess:
     
-    def userInList():
-        pass
+    def userInList(us,psw):
+        l = []
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="",
+            database="magicpotion") 
+        mycursor = mydb.cursor()
+        query=f"""SELECT * FROM Users ORDER BY id asc"""
+        mycursor.execute(query)
+        result=mycursor.fetchall()
+        print(query)
+        print(result)
+        lst=[]
+        users=[]
+        for r in result:
+            for i in range(0,3):
+                lst.append(r[i])
+            obj=user(lst[0],lst[1],lst[2])
+            users.append(obj)
+        logged=False
+        for usr in users:
+            if usr._is_logged(us,psw):        
+                logged=True
+        lst.clear()
+        return logged
 
     def _add_ingredient(a):
         mydb = mysql.connector.connect(
@@ -73,6 +98,11 @@ class DbAccess:
         print(result)
 
     def _see_potions():
-        l=[]
+        l = []
         return l
+    
+    def _modify_potions():
+        pass
+    def _delete_potions():
+        pass
 
