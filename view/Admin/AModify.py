f88071295a6a7961ed2041ef217bca2ae4fe0a78
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2022-11-01 11:04:55 +0100

    Add files via upload

--- view/Admin/AModify.py
+++ view/Admin/AModify.py
@@ -3,9 +3,10 @@ import  tkinter as tk
 
 from controller.dbAccess import DbAccess
 
-class ADelete(tk.Frame):
-    def __init__(self, parent, controller,win):
+class AModify(tk.Frame):
+    def __init__(self, parent, controller,win,ingWin):
         self.win=win
+        self._ing_Win=ingWin
         tk.Frame.__init__(self, parent)
         self.controller = controller
         self.bg= tk.PhotoImage(file="./img/sfondoBase.png")
@@ -20,24 +21,27 @@ class ADelete(tk.Frame):
     def _add_elements_(self):
         self._lb=tk.Listbox(self._canvas,selectmode=tk.SINGLE)
         self._add_ingredients_()
-        self._login = tk.Button(self,text="DELETE", width=20,command=self._start_,font=("Arial",15))
-        self._menu = tk.Button(self,text="Menu", width=20,command=self._menu_,font=("Arial",15))
+        self._login = tk.Button(self,text="EDIT", width=20,command=self._start_,font=("Arial",15))
         self._exitButton = tk.Button(self,text="exit", width=20,command=self._exit_,font=("Arial",15))
 
     def _reinitialize_lb_(self):
         self._lb.delete(0,tk.END)
         self._add_ingredients_()   
 
-    def _menu_(self):
-        self.controller.show_frame("MenuAdmin")
-    
     def _start_(self):
         sel=None
         for i in self._lb.curselection():
             sel=self._lb.get(i)
             val=sel.split(",")
-        idDel=val[0]
-        DbAccess._delete_ingredients(id=idDel)
+        id=val[0]
+        n=val[1]
+        f=val[2]
+        w=val[3]
+        a=val[4]
+        e=val[5]
+        self._ing_Win._set_values(id=id,f=f,n=n,w=w,a=a,e=e)
+        self.controller.show_frame("AIngredients")
+        
 
     def _setElements_(self):
         self._add_elements_()
@@ -45,9 +49,8 @@ class ADelete(tk.Frame):
         self._canvas.create_image(0,0,image=self.bg, anchor="nw")
         x=0.3
         self._lb.place(relx=0.15,y=20,relwidth=0.7)
-        self._login.place(relx=x, y=240,relwidth=0.4)
-        self._menu.place(relx=x,y=290,relwidth=0.4)
-        self._exitButton.place(relx=x, y=340,relwidth=0.4)  
+        self._login.place(relx=x, y=240)
+        self._exitButton.place(relx=x, y=290)  
 
     def _exit_(self):
         sys.exit()
