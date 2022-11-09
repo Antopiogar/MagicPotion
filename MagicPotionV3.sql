b23c7c975e3a49f0fa81212c656c4bc3f5e472fb
Antopiogar <103059144+Antopiogar@users.noreply.github.com>
2022-11-09 13:19:28 +0100

    Add files via upload

--- MagicPotionV3.sql
+++ MagicPotionV3.sql
@@ -4,18 +4,18 @@ USE magicPotion ;
 
 CREATE TABLE users (
 	id int PRIMARY KEY AUTO_INCREMENT ,
-    username varchar (25) NOT null,
-    pass varchar (25) NOT null
+    username varchar (255) NOT null,
+    pass varchar (255) NOT null
 );
 
 CREATE TABLE potions (
 	id int PRIMARY KEY AUTO_INCREMENT ,
-    name varchar (25) NOT null unique
+    name varchar (255) NOT null unique
 );
 
 CREATE TABLE ingredients (
 	id int PRIMARY KEY AUTO_INCREMENT ,
-    nome varchar (25) NOT null,
+    name varchar (255) NOT null,
     fire int not null,
     air int not null,
     water int not null,
@@ -29,13 +29,3 @@ CREATE TABLE pozioni_ingredienti (
     FOREIGN KEY (fk_ingredients) REFERENCES ingredients(id),
     PRIMARY KEY (fk_potions,fk_ingredients) 
 );
-
-INSERT INTO `ingredients`( `name`, `fire`, `air`, `water`, `earth`) VALUES (
-    ('Pescepalla',4,7,99,0),
-    ('Occhio di ragno',0,12,37,3),
-    ('Ram di Inf2',99,99,12,0),
-    ('Piede di coniglio',0,37,58,33),
-    ('polvere di Luminite',29,69,70,99),
-    ('Anima di Admin',99,99,99,99),
-    ('marmellata dimenticata nel frigo da un anno',0,69,99,40)
-   )
